b2dbbd6b0c38486fa4a12af9f36fa6c0502bafa5
nd-nagahori <nagahori@ndcube.co.jp>
2021-01-02 20:15:08 +0900

    コメントで補足する

--- TutorialPhoton2/Assets/Scripts/Launcher.cs
+++ TutorialPhoton2/Assets/Scripts/Launcher.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 
+/// <summary>
+/// ロビー画面
+/// 
+/// controlPanelとprogressLabelの表示制御はお互い排他になっている
+/// controlPanelがONのとき、通信前
+/// progressLabelがONのとき、通信中
+/// ※通信後は、違うシーンに飛ばされる
+/// 
+/// PhotonNetwork.IsConnected は PhotonNetwork.ConnectUsingSettings() を呼び出すまでfalseになっている
+/// 
+/// 
+/// かんたんな処理の流れ
+/// 「Play」の押下によって、Connectを呼び出す
+///     JoinRandomRoomしようとして、誰もいない、もしくは人数オーバー
+///         OnJoinRandomFailedがコールされる
+///             ルームを生成を試みる（OnJoinRandomFailed）
+///                 ルームを生成する
+/// </summary>
 public class Launcher : MonoBehaviourPunCallbacks
 {
     #region Private Serialize Fields
     [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
     [SerializeField]
-    private byte maxPlayersPerRoom = 4;
+    private byte maxPlayersPerRoom = 4; //  １ROOMの最大参加人数
 
     [Tooltip("The Ui Panel to let the user enter name, connect and play")]
     [SerializeField]
     private GameObject controlPanel;
+
     [Tooltip("The UI Label to inform the user that the connection is in progress")]
     [SerializeField]
     private GameObject progressLabel;
@@ -34,11 +53,19 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     #endregion
 
+    /// <summary>
+    /// Awake
+    /// Photonのシーン同期を有効化する
+    /// </summary>
     void Awake()
     {
-        PhotonNetwork.AutomaticallySyncScene =true;
-    }
-    // Start is called before the first frame update
+        PhotonNetwork.AutomaticallySyncScene = true;
+        }
+    
+    /// <summary>
+    /// Start
+    /// ロビー用UIを初期化する
+    /// </summary>
     void Start()
     {
         //Connect();
@@ -48,19 +75,19 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    /// <summary>
+    /// サーバへ接続を試みる
+    /// </summary>
     public void Connect()
     {
+        //  UIを通信中へ変更する
         progressLabel.SetActive(true);
         controlPanel.SetActive(false);
 
+        //  サーバへ接続してみる
+        //  始めはelse節へいく
         // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
-        if (PhotonNetwork.IsConnected)
+        if (PhotonNetwork.IsConnected)  //  過去にサーバへ接続したことがあればIsConncectedはtrueになっているので、適当なルームへJoinしようとする
         {
             // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
             PhotonNetwork.JoinRandomRoom();
@@ -68,7 +95,7 @@ public class Launcher : MonoBehaviourPunCallbacks
         else
         {
             // keep track of the will to join a room, because when we come back from the game we will get a callback that we are connected, so we need to know what to do then
-            isConnecting= PhotonNetwork.ConnectUsingSettings();
+            isConnecting = PhotonNetwork.ConnectUsingSettings();
 
             // #Critical, we must first and foremost connect to Photon Online Server.
             PhotonNetwork.GameVersion = gameVersion;
@@ -78,6 +105,11 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     #region MonoBehaviourPunCallbacks Callbacks
 
+    /// <summary>
+    /// マスターと接続完了コールバック
+    /// 
+    /// 接続の履歴があるかをチェックして、適当なルームへJoinする
+    /// </summary>
     public override void OnConnectedToMaster()
     {
         // we don't want to do anything if we are not attempting to join a room.
@@ -91,7 +123,10 @@ public class Launcher : MonoBehaviourPunCallbacks
         }
     }
 
-
+    /// <summary>
+    /// 通信切断コールバック
+    /// </summary>
+    /// <param name="cause"></param>
     public override void OnDisconnected(DisconnectCause cause)
     {
         progressLabel.SetActive(false);
@@ -103,6 +138,13 @@ public class Launcher : MonoBehaviourPunCallbacks
     }
     #endregion
 
+    /// <summary>
+    /// ジョイン失敗時コールバック
+    /// 
+    /// 人数オーバーか、部屋がないときは新しく部屋を生成する
+    /// </summary>
+    /// <param name="returnCode"></param>
+    /// <param name="message"></param>
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
@@ -112,6 +154,11 @@ public class Launcher : MonoBehaviourPunCallbacks
         PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
     }
 
+    /// <summary>
+    /// 部屋に入れたとき（開設時含む）コールバック
+    /// 
+    /// 一人目の場合は、待ちぼうけ用の部屋へ飛ばす（Room for 1.unity）
+    /// </summary>
     public override void OnJoinedRoom()
     {
         Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
